104b6b3a0328b72c25d8edbfa4708cc04cc63bdd
oguramari <oguramari1110@gmail.com>
2022-08-07 13:32:00 +0900

    DB変更
    H2DB→postgresqlへ変更。

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,22 @@
+CREATE DATABASE testdb;
+
 /* ユーザーマスタ */
 CREATE TABLE IF NOT EXISTS demo_user (
-                                         user_id VARCHAR(50) PRIMARY KEY
-    ,password VARCHAR(100)
-    ,user_name VARCHAR(50)
-    ,birthday DATE
-    ,age INT
-    ,gender INT
-    ,department_id INT
-    ,role VARCHAR(50)
-);
+    user_id character varying(50) NOT NULL,
+    password character varying(100),
+    user_name character varying(50),
+    birthday date,
+    age smallint,
+    gender smallint,
+    department_id smallint,
+    role character varying(50),
+    PRIMARY KEY(user_id),
+    UNIQUE(user_id)
+    );
 
 /* 部署マスタ */
 CREATE TABLE IF NOT EXISTS demo_department (
-                                               department_id INT PRIMARY KEY
-    ,department_name VARCHAR(50)
+    department_id smallint NOT NULL,
+    department_name character varying(50),
+    PRIMARY KEY(department_id)
 );
